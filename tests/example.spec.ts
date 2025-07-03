4f318b646b03c6bdfdf86f9bed7378a1988fa310
Iulian Pocotila <iulian.pocotila@gmail.com>
2025-07-03 16:18:15 +0300

    First commit

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+npx import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
@@ -7,12 +7,4 @@ test('has title', async ({ page }) => {
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+test('get started link', {tag: ['@smoke']}, async ({ page }) => {  await page.goto('https://www.saucedemo.com/');  await page.getByPlaceholder('Username').fill('standard_user');  await page.getByRole('textbox', {name: 'password'}).fill('secret_sauce');  await page.locator('xpath = /html/body/div/div/div[2]/div[1]/div/div/form/input').click();  await page.waitForTimeout(1000);  await expect(page.locator('.app_logo', { hasText: 'Swag Labs' })).toBeVisible();  await page.waitForTimeout(5000);});
